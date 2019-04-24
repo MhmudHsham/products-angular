5207086816ba5cf150fdfaf1acbf9a0610485129
Mahmoud Gabr <mhmudhsham8@gmail.com>
2019-04-24 18:46:21 +0400

    New Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TopHeaderComponent } from './core/top-header/top-header.component';
 import { HeaderComponent } from './core/header/header.component';
-import { NavbarComponent } from './core/navbar/navbar.component';
+import { TopHeaderComponent } from './core/header/top-header/top-header.component';
+import { MiddleHeaderComponent } from './core/header/middle-header/middle-header.component';
+import { EndHeaderComponent } from './core/header/end-header/end-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopHeaderComponent,
     HeaderComponent,
-    NavbarComponent
+    TopHeaderComponent,
+    MiddleHeaderComponent,
+    EndHeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/core/header/end-header/end-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-end-header',
+  templateUrl: './end-header.component.html',
+  styleUrls: ['./end-header.component.sass']
+})
+export class EndHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
